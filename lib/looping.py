aff43cb7d020d599a71399d2091d7f7d47d773ea
Meaghan <meaghan.sharrard89@gmail.com>
2023-12-10 10:09:44 -0800

    passing tests

--- lib/looping.py
+++ lib/looping.py
@@ -1,13 +1,31 @@
 #!/usr/bin/env python3
 
+
 def happy_new_year():
-    # code goes here!
+    count = 10
+    while count > 0:
+        print(count)
+        count -= 1
+    print("Happy New Year!")
     pass
+
 
 def square_integers(int_list):
-    # code goes here!
+    squared_int = [int**2 for int in int_list]
+    return squared_int
     pass
 
+
 def fizzbuzz():
-    # code goes here!
+    i = 0
+    while i < 100:
+        i += 1
+        if i % 3 == 0 and i % 5 == 0:
+            print("FizzBuzz")
+        elif i % 3 == 0:
+            print("Fizz")
+        elif i % 5 == 0:
+            print("Buzz")
+        else:
+            print(i)
     pass
